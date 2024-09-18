f79f5efd2d88025b65a447a5cbf1edaa983ee096
Aleksander-hub <oshepkov77@gmail.com>
2024-09-18 17:37:17 +0300

    Add files via upload

---
+++ urok_dz 2.py
@@ -0,0 +1,8 @@
+
+
+first_name='Aleksander'
+dz=12
+kol_Z_time=1.5
+name_kurs='python'
+time_z_zadanie=kol_Z_time/dz
+print('Курс:',name_kurs,first_name,'всего задач:',dz,',затрачено часов:',kol_Z_time,'среднее время выполнения',time_z_zadanie,'часа.',)
