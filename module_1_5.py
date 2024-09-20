fb681ba058f72a791397e559eff6909f68532449
Aleksander-hub <oshepkov77@gmail.com>
2024-09-20 05:49:18 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var=(5,9,'b','c')
+print(immutable_var)
+print('кортеж это не изменяемый тип данных')
+print('но может содержать изменяемй тип данных: [a],[b]')
+immutable_list=[10,20,'c','b']
+print(immutable_list)
+immutable_list[0]=50
+immutable_list[2]='a'
+print(immutable_list)
