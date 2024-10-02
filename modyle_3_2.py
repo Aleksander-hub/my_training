47272f50dc9caa8974cb8ee43e920152b4717d3a
Aleksander-hub <oshepkov77@gmail.com>
2024-10-02 15:47:06 +0300

    Update modyle_3_2.py

--- modyle_3_2.py
+++ modyle_3_2.py
@@ -1,14 +1,20 @@
 
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    print(message, recipient, sender)
     if "@" not in sender or not sender.endswith((".com", ".ru", ".net")):
-        print(message, recipient, sender)
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
-    
+    if "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
 
-send_email("Письмо успешно отправлено с адреса", "university.help@gmail.com", sender='vasyok1337@gmail.com')
-send_email('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!', 'urban.info@mail.ru', sender='urban.fan@gmail.com')
-send_email("Невозможно отправить письмо с адреса","urban.teacher@mail.uk",sender="urban.student@mail.ru")
+send_email('Это сообщение для проверки связи', 'n.tililitsin7@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
