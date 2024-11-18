bd9d51c5ddaee5c8bfb6160d9a23fc65fd447953
Aleksander-hub <oshepkov77@gmail.com>
2024-11-18 19:10:01 +0300

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -10,3 +10,4 @@ def apply_all_func(int_list, *functions):
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([6, 20, 15, 9], any, format))
