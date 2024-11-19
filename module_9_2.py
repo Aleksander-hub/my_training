867d89ebe75af1ce5fe27b156df21d872f1eef61
Aleksander-hub <oshepkov77@gmail.com>
2024-11-19 10:46:06 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,10 @@
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings if len(x) > 5]
+print(first_result)
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+print(second_result)
+third_result = {x: len(x) for x in first_strings + second_strings if not len(x) % 2}
+print(third_result)
