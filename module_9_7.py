479fab70a7dc6b10df493647a764c6a916d25057
Aleksander-hub <oshepkov77@gmail.com>
2024-11-25 06:26:51 +0300

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -2,15 +2,15 @@
 def is_prime(func):
     def wrapper(*args):
         res = func(*args)
-        log = True
-        for i in range(1, res):
+
+        for i in range(2, res):
             if res % i == 0:
-                log = False
-        if log == True:
-            print('Составное число')
-        else:
-            print('Простое число')
-        return res
+                print('Составное число')
+                return res
+            else:
+                print('Простое число')
+                return res
+
     return wrapper
 
 
