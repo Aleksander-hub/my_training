836dec11f970ed058aa4ee65c63b49779eed371a
Aleksander-hub <oshepkov77@gmail.com>
2025-01-07 08:44:54 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -2,18 +2,20 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "key" # ID ключ
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+api = "BOT TOKEN HERE" 
 bot = Bot(token= api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
 
-# данный handler реагирует на команду /start
+# Этот хэндлер будет срабатывать на команду "/start"
 @dp.message_handler(commands=['start'])
 async def start(message):
     print("Привет! Я бот помогающий твоему здоровью.")
 
 
-# данный handler реагирует на все сообщения
+# Этот хэндлер будет срабатывать на остальные любые сообщения
 @dp.message_handler()
 async def all_massages(message):
     print("Введите команду /start, чтобы начать общение.")
