2dddf029d74b102814b13ac332fd2b074a554bc5
Aleksander-hub <oshepkov77@gmail.com>
2025-01-08 07:46:22 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+api = "BOT TOKEN HERE"
+bot = Bot(token= api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message_handler(commands=['start'])
+async def start(message):
+
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+# Этот хэндлер будет срабатывать на остальные любые сообщения
+@dp.message_handler()
+async def all_massages(message):
+
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
