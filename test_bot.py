ee46aa4892ac20aaeaf3a9d4e7d319cd02533a07
Aleksander-hub <oshepkov77@gmail.com>
2025-01-13 07:41:34 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7266456982:AAHD3565fBvroH2CqC3dHrOKO7nquuQeQzI"
+api = ""
 bot = Bot(token= api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
