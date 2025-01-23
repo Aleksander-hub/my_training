bf3e05e58a722ee9d2201aac6566543989d5e99e
Aleksander-hub <oshepkov77@gmail.com>
2025-01-23 10:44:20 +0300

    Add files via upload

---
+++ main_0.py
@@ -0,0 +1,18 @@
+
+print("1st program")
+print(9 ** 0.5 * 5)
+
+print("2nd program")
+print( 9.99 > 9.98 and 1000 != 1000.1 )
+
+print("3rd program")
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+print("4th program")
+a = "123.456"
+print(a[4])
+
+
+s = "123.456"
+num = float(s)
+print(num * 10)
